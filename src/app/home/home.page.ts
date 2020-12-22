fdc44ff80dacb264e0a6bb782a631c1ac5973c46
Hemjie <marjolaine.vilain@gmail.com>
2020-12-22 15:47:17 +0100

    affichage des photos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera } from '@ionic-native/camera/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  listImages = [];
 
-  constructor() {}
+  constructor(
+    private camera: Camera,
+    private alert: AlertController
+    ) {}
+
+  //faire appel au service camera pour utiliser l'appareil photo
+  takePicture() {
+    //on dit que l'on veut l'image au format base 64 au lieu d'un fichier
+    let options = {
+      destinationType : this.camera.DestinationType.DATA_URL
+    };
+    this.camera.getPicture(options).then(image => {
+      //on récupère la photo dans image
+      this.listImages.push('data:image/png;base64,'+image);
+
+      //on crée une alerte pour débugger l'image
+      this.alert.create({ //retourne une promesse
+        message: 'Salut! '+image
+      }).then(alert => alert.present()); //affiche l'alerte
+    });
+  }
 
 }
